8bc77dd582688785b68926eb9b3c91ca593e63a4
agustinperalta <43344932+agustinperalta@users.noreply.github.com>
2020-04-07 17:18:10 -0300

    Updates

---
+++ precache-manifest.f0b4573b2e79c260777fdee43eb89151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e9f179587c19db37d9539b39271d40",
+    "url": "/Curso-Udemy-ReactJs/index.html"
+  },
+  {
+    "revision": "84e4c41ab2748d7fd658",
+    "url": "/Curso-Udemy-ReactJs/static/css/main.8cfe6a5d.chunk.css"
+  },
+  {
+    "revision": "4613b55f5280d8c21bad",
+    "url": "/Curso-Udemy-ReactJs/static/js/2.bc909987.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Curso-Udemy-ReactJs/static/js/2.bc909987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e4c41ab2748d7fd658",
+    "url": "/Curso-Udemy-ReactJs/static/js/main.99365311.chunk.js"
+  },
+  {
+    "revision": "faefc09b0dec4e55812c",
+    "url": "/Curso-Udemy-ReactJs/static/js/runtime-main.7762604a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Curso-Udemy-ReactJs/static/media/logo.ee7cd8ed.svg"
+  }
+]);
